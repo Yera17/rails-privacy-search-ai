92a8c2ea2c697c8d5619193dafd0b97ead9c05c4
Vidya V <vidya1141@gmail.com>
2024-09-02 17:21:58 +0200

    code after doing the first 2 pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Source.create(identification_method: "CEO of Consumer Lending, focusing on growing lending and deposits.", identified_text: "We had a very strong commercial performance in [DATE_INTERVAL_2] with an increase in the number of customers, in lending and in deposits.", person_id: 1);
