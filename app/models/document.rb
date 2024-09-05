b5fb13ccd517599965a45ff031a9e21fdeb0f0ff
Yerkanat Salaly <erko.salaly@gmail.com>
2024-09-05 14:24:11 +0200

    newThursday

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,5 @@
 class Document < ApplicationRecord
   belongs_to :user
-  has_many :queries, dependent: :destroy
-
+  has_many :people, dependent: :destroy
   validates :file_name, presence: true
 end
