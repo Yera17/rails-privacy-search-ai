329cf90d59d8a0b931cf040d9319216d6a2994d4
Yerkanat Salaly <erko.salaly@gmail.com>
2024-09-05 12:38:42 +0200

    thursday

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :documents, dependent: :destroy
   validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+  validates :email, acceptance: { accept: ['erko.salaly@gmail.com', 'vidya1141@gmail.com', 'jellesijm1@gmail.com', 'syilagan@icloud.com'] }
 end
